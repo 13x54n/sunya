5a8193fde51e2eeac8101e9c15d418de705613cd
codemonkey-sagar <fmtsagar@gmail.com>
2024-06-15 15:03:54 -0400

    Dynamic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  DynamicContextProvider,
+  DynamicWidget,
+} from '@dynamic-labs/sdk-react-core';
+import { EthersExtension } from '@dynamic-labs/ethers-v5';
 
-function App() {
-  const [count, setCount] = useState(0)
+import { EthereumWalletConnectors } from '@dynamic-labs/ethereum';
+import { StarknetWalletConnectors } from '@dynamic-labs/starknet';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => (
+  <DynamicContextProvider
+    settings={{
+      environmentId: 'c991ecb6-69ad-457f-b187-077b666b3f12',
+      walletConnectorExtensions: [EthersExtension],
+      walletConnectors: [EthereumWalletConnectors, StarknetWalletConnectors],
+    }}
+  >
+    <DynamicWidget />
+  </DynamicContextProvider>
+);
 
-export default App
+export default App;
