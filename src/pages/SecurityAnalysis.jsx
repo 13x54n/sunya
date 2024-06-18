b05a22f6d372d673a3caed582a7821b36467a352
13x54n <laxman.rai.07.26@gmail.com>
2024-06-17 20:11:14 -0400

    📜 documentation: added readme for project installation.

--- src/pages/SecurityAnalysis.jsx
+++ src/pages/SecurityAnalysis.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SecurityAnalysis() {
+    const [activeAnalysis, setActiveAnalysis] = useState("starknet")
   return (
-    <div>
-      Security Analysis
+    <div className='py-3 px-2'>
+      <div className='inline-flex overflow-hidden items-center shadow-lg border-2 border-gray-500 text-sm rounded-xl bg-gray-50'>
+        <div className={`p-2 ${activeAnalysis === "starknet" && 'bg-gray-500 text-white'}`}>Starknet</div>
+        <div className={`p-2 ${activeAnalysis === "ethereum" && 'bg-gray-500 text-white'}`}>Ethereum</div>
+      </div>
     </div>
   )
 }
